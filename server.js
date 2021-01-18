132d451ee6059857865f6c3d49f33559c909bafb
emispalenza <emispalenza@gmail.com>
2021-01-18 18:29:09 -0300

    conexión a bd mongo

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const app = express();
 
 app.get('/api/test', cors(), (req, res) => {
-  const test = [
-    {id: 1, firstName: 'Cristian', lastName: 'Machuca'},   
-  ];
+  const test = [{ id: 1, firstName: 'Cristian', lastName: 'Machuca' }];
   res.json(test);
 });
 
 const port = 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
+
+
+mongoose.Promise = global.Promise;
+
+// Usamos el método connect para conectarnos a nuestra base de datos
+mongoose.connect('mongodb://localhost:27017/jr-devs', {useMongoClient: true})
+        .then(() => {
+            console.log('La conexión a MongoDB se ha realizado correctamente!!');
+        })
+        .catch(err => console.log(err));
+
+
+
+
